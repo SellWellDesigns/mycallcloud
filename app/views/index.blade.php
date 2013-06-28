62c3f012ef3f932dbbffa9f1be4da2fdfcba1932
Conar Welsh <conar@sellwelldesigns.com>
2013-06-28 16:01:47 -0600

    content changes

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -16,125 +16,51 @@
 				Telephony with Integrity
 			</p>
 			<p>
-				
+				MyCallCloud is a full-service software-consulting firm that empowers your business with the knowledge, technology and services to reach your customers with the right message at the right time.
 			</p>
 			<p>
-				
+				Our team was raised in the communications and technology industries; this has been and will be our professional passion.				
+			</p>
+			<p>
+				By applying our knowledge of the industry into our own technology we have built and immensely robust system that is responsive across devices and was designed with the everyday user in mind.  Every employee in your business; from the C-Suites to the call center operator will find enormous functionality, powerful analytics, and actionable reports.  Simplifying workflow, increasing efficiency.
 			</p>
 		</div>
 	</div>
 
 	<div id="services" class="homepage-section container">
 		<div class="row">
-			<div class="col col-lg-4 text-center">
+			<div class="col col-lg-3 text-center">
 				<i class="glyphicon glyphicon-refresh"></i>
-				<h3>Responsive Design</h3>
+				<h3>Consulting</h3>
 				<p>
-					Our website is an example of a responsive design; try resizing the browser to watch how the website responds to a different viewport size. Responsive design ensures that your website will provide the best viewing experience, regardless of what type of device it is viewed from: Desktop, laptop, tablet, or mobile.
+					MyCallCloud knows that our success is fully dependent on providing our customers with the tools and practices to resolve even your most complex problems.  Our team was raised in this field and understands the issues at hand.  Working in conjunction with our technology products the MyCallCloud team will find the answer you need.
 				</p>
 				<hr class="visible-sm" />
 			</div>
-			<div class="col col-lg-4 text-center">
+			<div class="col col-lg-3 text-center">
 				<i class="glyphicon glyphicon-globe"></i>
-				<h3>Usability</h3>
+				<h3>PBX</h3>
 				<p>
-					When powerful functionality meets intelligent design a product becomes truly usable. We consider ourselves students of usability; our love lays in the never-ending chase of its perfection. Usability ensures that regardless if it is the CEO or the first time visitor, their experience with the product is natural and effective.
+					MyCallCloud Private Branch Exchange contains the capabilities to be the most powerful Private Branch Exchange in the cloud.  Inbound/Outbound dialing, mobile integration, video capabilities, SMS, and Click to Call are just a handful of features you’ll find in our proprietary technology.
 				</p>
 				<hr class="visible-sm" />
 			</div>
-			<div class="col col-lg-4 text-center">
+			<div class="col col-lg-3 text-center">
 				<i class="glyphicon glyphicon-retweet"></i>
-				<h3>Agile Workflow</h3>
+				<h3>VoIP</h3>
 				<p>
-					A house is not built in a single step, nor should a website! Construction of a house is done one phase at a time, followed by an inspection to ensure quality. Construction of a premium website is analogous; by breaking the development process down into several phases with each being followed up by an inspection by the client.
+					Our experience working with VoIP is as old as VoIP itself, we’ve paid our dues and have the connections to back it up!  Our seven national carrier networks reach when combined with our LocalTouch technology allows you to stay in contact with your customers regardless of region and with a local number.
+				</p>
+			</div>
+			<div class="col col-lg-3 text-center">
+				<i class="glyphicon glyphicon-music"></i>
+				<h3>Call Center</h3>
+				<p>
+					The MyCallCloud Call Center is an amazingly robust customizable suite of features. Incorporating blended dialing along side CRM integration through an open API. Personalized dashboards derived from tested analytics create actionable reports from the operator to the manager, and Direct to the C Suite. 
 				</p>
 			</div>
 		</div>
 	</div>	
-
-	<div id="team" class="homepage-section container">
-		<h1>Meet Our Team</h1>
-
-		<div class="row">
-			<div class="col col-lg-3 text-center">
-				<img src="{{ asset('img/conar.jpg') }}" class="img-circle" />
-				<p class="lead">
-					Conar Welsh
-					<small>
-						Founder<br />
-						Director of Web Development
-					</small>
-				</p>
-				<p>
-					Conar has experience designing custom web solutions for Fortune 500 companies, 
-					as well as start-ups and small businesses.  It is his passion for web development, 
-					the agile workflow model, and lean startup principles that led him to a single conclusion: 
-					that we could build better products, with better processes, and love doing it at every stage.  
-					Conar is an open-source evangelist and is a regular contributor to open-source projects.
-				</p>
-				<hr class="visible-sm" />
-			</div>
-			<div class="col col-lg-3 text-center">
-				<img src="{{ asset('img/barrett.jpg') }}" class="img-circle" />
-				<p class="lead">
-					Barrett Seller
-					<small>
-						Co-Founder<br />
-						Director of Marketing
-					</small>
-				</p>
-				<p>
-					Barrett was raised in Evergreen and Boulder County. 
-					His Degrees in Political Science and Psychology from the University 
-					of Colorado Denver naturally landed a position in marketing.  Pursuing 
-					this new field with passion he quickly advanced to Director of 
-					Marketing responsible for several medicinal brands.   Family and friends 
-					come first on his priority list but right on their heels are Broncos Football 
-					and Music at Red Rocks!
-				</p>
-				<hr class="visible-sm" />
-			</div>
-			<div class="col col-lg-3 text-center">
-				<img src="{{ asset('img/evan.jpg') }}" class="img-circle" />
-				<p class="lead">
-					Evan Dubicki
-					<small>
-						Partner<br />
-						Director of Sales
-					</small>
-				</p>
-				<p>
-					Evan grew up in the suburbs of Philadelphia.  
-					From a young age and thanks to one horrible boss, he knew 
-					he wanted to run his own business.  After receiving a Small 
-					Business Management degree he successfully started and sold 
-					two companies working with some of the world’s largest automakers.  
-					After moving to Colorado, a new challenge was needed; he is excited 
-					to utilize his skill set in the ever-changing world of web development.
-				</p>
-			</div>
-			<div class="col col-lg-3 text-center">
-				<img src="{{ asset('img/meaghan.jpg') }}" class="img-circle" />
-				<p class="lead">
-					Meaghan Mulcahy
-					<small>
-						Project Manager
-						<br />
-						&nbsp;
-					</small>
-				</p>
-				<p>
-					Meaghan was raised in Aurora, Colorado, 
-					but her roots and heart are in Honolulu, Hawai&#699;i.  
-					She earned her bachelors degree from Colorado State University, 
-					and is currently earning her masters degree at the University 
-					of Colorado.  Past project management experience in the web 
-					technology and development areas led her to join Sell Well Designs, LLC.  
-					Meaghan enjoys traveling the world, sleeping in late, and avoiding large crowds.
-				</p>
-			</div>
-		</div>
-	</div>
 @stop
 
 
